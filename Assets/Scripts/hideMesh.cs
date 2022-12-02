2cd7a7c11e0b24930c34aa7d9f0a3694fb510d45
Joshua Silva <joshuaarindha@gmail.com>
2022-12-03 00:17:28 +0300

    UI

--- Assets/Scripts/hideMesh.cs
+++ Assets/Scripts/hideMesh.cs
@@ -9,10 +9,19 @@ public class hideMesh : MonoBehaviour
    
     public void meshHide()
     {
+        // Use CoreServices to quickly get access to the IMixedRealitySpatialAwarenessSystem
+        var spatialAwarenessService = CoreServices.SpatialAwarenessSystem;
+
+        // Cast to the IMixedRealityDataProviderAccess to get access to the data providers
+        var dataProviderAccess = spatialAwarenessService as IMixedRealityDataProviderAccess;
+
+        var meshObserver = dataProviderAccess.GetDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+
         // Get the first Mesh Observer available, generally we have only one registered
         var observer = CoreServices.GetSpatialAwarenessSystemDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
 
         // Set to not visible
         observer.DisplayOption = SpatialAwarenessMeshDisplayOptions.None;
+       
     }
 }
