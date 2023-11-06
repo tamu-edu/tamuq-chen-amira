f346918e342be5918b547edf7a790b2069624610
Josh <joshsilva@tamu.edu>
2023-11-06 12:02:56 +0300

    local spatial anchors

--- Unreal/AMIRA/Source/AMIRA/AMIRAGameModeBase.cpp
+++ Unreal/AMIRA/Source/AMIRA/AMIRAGameModeBase.cpp
@@ -2,4 +2,83 @@
 
 
 #include "AMIRAGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
+
+void AAMIRAGameModeBase::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    Super::EndPlay(EndPlayReason);
+
+    // Gather all actors to save
+    TArray<AActor*> ActorsToSave;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), AActor::StaticClass(), ActorsToSave);
+
+    SaveActorTransforms(ActorsToSave);
+}
+
+void AAMIRAGameModeBase::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // Assuming you have a method to find/load the actors you want to set transforms for
+    TArray<AActor*> ActorsToLoad;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), AActor::StaticClass(), ActorsToLoad);
+
+    LoadActorTransforms(ActorsToLoad);
+}
+
+void AAMIRAGameModeBase::SaveActorTransforms(const TArray<AActor*>& ActorsToSave)
+{
+    FString SaveData;
+    for (AActor* Actor : ActorsToSave)
+    {
+        if (Actor)
+        {
+            // Convert each transform to a string and add it to the save data
+            FTransform ActorTransform = Actor->GetActorTransform();
+            SaveData += Actor->GetName() + " " + ActorTransform.ToString() + LINE_TERMINATOR;
+        }
+    }
+
+    // Save the string to a file
+    FString SaveDirectory = FPaths::ProjectSavedDir();
+    FString FileName = FString(TEXT("ActorTransformsSave.txt"));
+    FString FullPath = SaveDirectory + FileName;
+
+    FFileHelper::SaveStringToFile(SaveData, *FullPath);
+}
+
+void AAMIRAGameModeBase::LoadActorTransforms(TArray<AActor*>& ActorsToLoad)
+{
+    FString LoadDirectory = FPaths::ProjectSavedDir();
+    FString FileName = FString(TEXT("ActorTransformsSave.txt"));
+    FString FullPath = LoadDirectory + FileName;
+
+    FString LoadedData;
+    FFileHelper::LoadFileToString(LoadedData, *FullPath);
+
+    TArray<FString> Lines;
+    LoadedData.ParseIntoArrayLines(Lines);
+
+    for (FString& Line : Lines)
+    {
+        FString ActorName;
+        FString TransformString;
+
+        // Assuming the actor's name is first, followed by the transform
+        Line.Split(" ", &ActorName, &TransformString);
+
+        for (AActor* Actor : ActorsToLoad)
+        {
+            if (Actor && Actor->GetName() == ActorName)
+            {
+                FTransform ActorTransform;
+                ActorTransform.InitFromString(TransformString);
+                Actor->SetActorTransform(ActorTransform);
+                break;
+            }
+        }
+    }
+}
 
--- Unreal/AMIRA/Source/AMIRA/AMIRAGameModeBase.h
+++ Unreal/AMIRA/Source/AMIRA/AMIRAGameModeBase.h
@@ -13,5 +13,15 @@ UCLASS()
 class AMIRA_API AAMIRAGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+    virtual void BeginPlay() override;
+
+    // Function to save actors' transforms to a file
+        void SaveActorTransforms(const TArray<AActor*>&ActorsToSave);
+
+    // Function to load actors' transforms from a file and apply them
+    void LoadActorTransforms(TArray<AActor*>& ActorsToLoad);
 	
 };
